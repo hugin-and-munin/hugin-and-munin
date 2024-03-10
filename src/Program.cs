bbf4e0049d5ad11730e98e4acb9c0a17fbdd8f40
Aen Saevherne <fbar4841@gmail.com>
2024-03-10 18:18:36 +0100

    Fix gRPC

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,14 @@
 using HuginAndMunin;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using static CredChecker.CredChecker;
 using static LegalEntityChecker.LegalEntityChecker;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();
+
+builder.WebHost.ConfigureKestrel(kestrel =>
+    kestrel.ConfigureEndpointDefaults(listen => listen.Protocols = HttpProtocols.Http2));
 
 var config = builder.Configuration;
 var appOptions = config.GetSection(AppOptions.Name).Get<AppOptions>() ?? throw new InvalidOperationException();
@@ -13,12 +17,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpClient();
 builder.Services.AddHealthChecks().AddCheck<HealthCheck>("Health");
 builder.Services.Configure<AppOptions>(config.GetSection(AppOptions.Name));
-builder.Services.AddSingleton<HuginAndMunin.ReportProvider>();
+builder.Services.AddGrpc();
 builder.Services.AddGrpcClient<LegalEntityCheckerClient>(o => o.Address = new Uri(appOptions.LegalEntitiesCheckerUri));
 builder.Services.AddGrpcClient<CredCheckerClient>(o => o.Address = new Uri(appOptions.CredCheckerUri));
 
 var app = builder.Build();
 
+app.MapGrpcService<HuginAndMunin.ReportProvider>();
 app.MapHealthChecks("/health");
 
 await app.RunAsync();
